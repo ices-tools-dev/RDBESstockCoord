1961af732b2f266ba0753f83484102b09eae550c
Jonathan Stounberg <jostou@dtu.dk>
2026-03-02 11:23:37 +0100

    Update of examples, to accomodate function name changes, and folder structure changes

--- vignettes/Example_saithe_conversion.R
+++ vignettes/Example_saithe_conversion.R
@@ -17,7 +17,7 @@ library(stringr)
 
 ## Set appropriate WD, based on some usual EDI R-opening behaviours:
 if (basename(getwd()) == "R")
-    setwd("..")
+  setwd("..")
 
 ## Paths:
 path <- "./R" ##"./WGRDBESstockCoord/personal/jost"
@@ -30,43 +30,43 @@ dat_path <- "./WGRDBESstockCoord/personal/jost/data_overviews_format/pok"
 ## path_to_data <- "./WGRDBESstockCoord/personal/jost"
 #out_path <- "Q:/dfad/users/jostou/home/wg_stock/rcef_intercatch"
 
-source(file.path(path, "fun_ICout_to_RCEF.R"))
-source(file.path(path, "fun_make_relation.R"))
-source(file.path(path, "fun_intercatch_RCEF.R"))
+source(file.path(path, "funICoutCEF.R"))
+source(file.path(path, "funMakeRelation.R"))
+source(file.path(path, "funIntercatchCEF.R"))
 
 ## ##################################################
 ## Conversion to RCEF v16:
 
 ## To files:
-ICout_RCEF(dat_path = dat_path, 
-           years = 2022,
-           metier6 = "fleet",
-           output_format = "to_file",
-           out_path = dat_path,
-           keep_temp_file = TRUE,
-           file_prefix = "pok_2022_")
+funICoutCEF(dat_path = dat_path, 
+            years = 2022,
+            metier6 = "fleet",
+            output_format = "to_file",
+            out_path = dat_path,
+            keep_temp_file = TRUE,
+            file_prefix = "pok_2022_")
 
 ## Returned as a list:
-res <- ICout_RCEF(dat_path = dat_path, 
-                  years = 2022,
-                  metier6 = "fleet",
-                  output_format = "to_list",
-                  out_path = dat_path,
-                  keep_temp_file = TRUE,
-                  file_prefix = "")
+res <- funICoutCEF(dat_path = dat_path, 
+                   years = 2022,
+                   metier6 = "fleet",
+                   output_format = "to_list",
+                   out_path = dat_path,
+                   keep_temp_file = TRUE,
+                   file_prefix = "")
 
 attr(res, "RCEF_version")
 
 ## Take a peek at the results:
 res$catches %>%
-    group_by(catchCategory, sourceType, variableType) %>%
-    slice_head(n = 1) %>%
-    as.data.frame()
+  group_by(catchCategory, sourceType, variableType) %>%
+  slice_head(n = 1) %>%
+  as.data.frame()
 
 res$distributions %>%
-    group_by(catchCategory, variableType) %>%
-    slice_head(n = 2) %>%
-    as.data.frame()
+  group_by(catchCategory, variableType) %>%
+  slice_head(n = 2) %>%
+  as.data.frame()
 
 ## Explo composite key matching:
 res2 <- res$catches
@@ -78,9 +78,9 @@ table(res2$domainCatchBMS, res2$catchCategory)
 ##   reported in InterCatch for combinations without landings
 ##   (mostly Sweden and Scotland); for instance:
 res2 %>%
-    filter(vesselFlagCountry == "UK(Scotland)",
-           areaValue == "27.6.a",
-           metier6 == "OTB_DEF_>=120_0_0_all") %>%
-    as.data.frame()
+  filter(vesselFlagCountry == "UK(Scotland)",
+         areaValue == "27.6.a",
+         metier6 == "OTB_DEF_>=120_0_0_all") %>%
+  as.data.frame()
 
 ## Note landings and discards reported at the year level, while BMS per quarter!
--- vignettes/example_Intercatch_to_RCEF.R
+++ vignettes/example_Intercatch_to_RCEF.R
@@ -1,19 +1,18 @@
 
 library(data.table)
 library(icesVocab)
-library(RstoxData)
-library(tidyr)
-library(plyr)
+library(stringr)
+
+## Paths:
+path <- "./R" 
+dat_path <- "./WGRDBESstockCoord/personal/jost/data_exchange_format"
 
 
-path <- "Q:/20-forskning/20-dfad/users/jostou/home/wg_stock/WGRDBESstockCoord/personal/jost"
-dat_path <- "Q:/20-forskning/20-dfad/users/jostou/home/wg_stock/WGRDBESstockCoord/personal/jost/data_exchange_format"
-#out_path <- "Q:/dfad/users/jostou/home/wg_stock/rcef_intercatch"
+source(file.path(path, "funMakeRelation.R"))
+source(file.path(path, "funIntercatchCEF.R"))
 
-source(paste0(path, "/fun_make_relation.R"))
-source(paste0(path, "/fun_intercatch_RCEF.R"))
+stock_relation <- funMakeRelation(year(Sys.Date()))
 
-stock_relation <- makeRelation(year(Sys.Date()))
-convExcahcnge(dat_path = dat_path, 
-              stock_relation = stock_relation, 
-              output_format = "to_environment")
+funIntercatchCEF(dat_path = dat_path, 
+                        stock_relation = stock_relation, 
+                        output_format = "to_environment")
