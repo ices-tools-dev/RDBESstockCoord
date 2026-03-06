23d98b122ae0173d76f49292fefed310cb418c13
Yves Reecht <Yves.Reecht@hi.no>
2026-03-06 10:23:29 +0100

    Update the NS saithe conversion example with mock data
    Scrambled/resampled data still need to be made more realistic
    in terms of consistency.

--- vignettes/Example_saithe_conversion.R
+++ vignettes/Example_saithe_conversion.R
@@ -16,44 +16,48 @@ library(stringr)
 
 
 ## Set appropriate WD, based on some usual EDI R-opening behaviours:
-if (basename(getwd()) == "R")
+if (basename(getwd()) == "R" ||
+    basename(getwd()) == "vignettes")
     setwd("..")
 
 ## Paths:
-path <- "./R" ##"./WGRDBESstockCoord/personal/jost"
+scriptPath <- "./R" ##"./WGRDBESstockCoord/personal/jost"
 
 ## NS saithe data (CATON and samples randomized,
 ##   so expect some inconsistencies in caton for a same stratum among tables,
 ##   or samples where zero-catch!):
-dat_path <- "./WGRDBESstockCoord/personal/jost/data_overviews_format/pok"
+dat_path <- "./data/data_overviews_format/pok"
+dat_out_path <- "./data/data_CEF_format/pok"
 
-## path_to_data <- "./WGRDBESstockCoord/personal/jost"
+dir.create(path = dat_out_path, recursive = TRUE)
+
+## scriptPath_to_data <- "./WGRDBESstockCoord/personal/jost"
 #out_path <- "Q:/dfad/users/jostou/home/wg_stock/rcef_intercatch"
 
-source(file.path(path, "fun_ICout_to_RCEF.R"))
-source(file.path(path, "fun_make_relation.R"))
-source(file.path(path, "fun_intercatch_RCEF.R"))
+source(file.path(scriptPath, "funICoutCEF.R"))
+source(file.path(scriptPath, "funMakeRelation.R"))
+source(file.path(scriptPath, "funIntercatchCEF.R"))
 
 ## ##################################################
 ## Conversion to RCEF v16:
 
 ## To files:
-ICout_RCEF(dat_path = dat_path, 
-           years = 2022,
-           metier6 = "fleet",
-           output_format = "to_file",
-           out_path = dat_path,
-           keep_temp_file = TRUE,
-           file_prefix = "pok_2022_")
+funICoutCEF(dat_path = dat_path, 
+            years = 2022,
+            metier6 = "fleet",
+            output_format = "to_file",
+            out_path = dat_out_path,
+            keep_temp_file = TRUE,
+            file_prefix = "pok_2022_")
 
 ## Returned as a list:
-res <- ICout_RCEF(dat_path = dat_path, 
-                  years = 2022,
-                  metier6 = "fleet",
-                  output_format = "to_list",
-                  out_path = dat_path,
-                  keep_temp_file = TRUE,
-                  file_prefix = "")
+res <- funICoutCEF(dat_path = dat_path, 
+                   years = 2022,
+                   metier6 = "fleet",
+                   output_format = "to_list",
+                   out_path = dat_out_path,
+                   keep_temp_file = TRUE,
+                   file_prefix = "")
 
 attr(res, "RCEF_version")
 
@@ -74,13 +78,13 @@ res2 <- res$catches
 table(res2$domainCatchDis, res2$catchCategory)
 table(res2$domainCatchBMS, res2$catchCategory)
 
-## Some discrepancies because of (mostly zero-)Registered discards/BMS
-##   reported in InterCatch for combinations without landings
-##   (mostly Sweden and Scotland); for instance:
-res2 %>%
-    filter(vesselFlagCountry == "UK(Scotland)",
-           areaValue == "27.6.a",
-           metier6 == "OTB_DEF_>=120_0_0_all") %>%
-    as.data.frame()
+## ## Some discrepancies because of (mostly zero-)Registered discards/BMS
+## ##   reported in InterCatch for combinations without landings
+## ##   (mostly Sweden and Scotland); for instance:
+## res2 %>%
+##     filter(vesselFlagCountry == "UK(Scotland)",
+##            areaValue == "27.6.a",
+##            metier6 == "OTB_DEF_>=120_0_0_all") %>%
+##     as.data.frame()
 
 ## Note landings and discards reported at the year level, while BMS per quarter!
